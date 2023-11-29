0c78f00b62a6824c17d3c24247932b54ff843b16
Ketbome <pims.2711@gmail.com>
2023-11-28 22:10:41 -0300

    fix: :bug: Correccion de grid ultimo y mejor

--- GridSearch ultimo.py
+++ GridSearch ultimo.py
@@ -10,6 +10,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
+# Importa los optimizadores
+from tensorflow.keras.optimizers import Adam, RMSprop, SGD
+
+import time
+
+start_time = time.time()
 
 # Crear la carpeta 'resultados' si no existe
 if not os.path.exists('resultados'):
@@ -39,6 +45,10 @@ correlacion_all = data_all[numeric_columns].corr()
 correlacion_prom_gral = correlacion_all["PROM_GRAL"].sort_values(
     ascending=False)
 
+# Obtener nombres de las columnas de correlacion distintas que no sean Nan
+columnas_seleccionadas = correlacion_prom_gral[correlacion_prom_gral.notna(
+)].index
+
 # Obtén los nombres de las columnas con correlación mayor a 0.1
 columnas_mayor_0_1 = correlacion_prom_gral[correlacion_prom_gral > 0.057].index
 
@@ -50,12 +60,12 @@ columnas_seleccionadas = columnas_mayor_0_1.tolist() + \
 
 # Agregar comuna del colegio
 columnas_seleccionadas.append('COD_COM_RBD')
-# columnas_seleccionadas.append('DGV_RBD')
 
-# Hacer data = data_all con las columnas a usar EDAD_ALU, GEN_ALU, PROM_GRAL
-# data = data_all[['COD_COM_RBD', 'COD_DEPE2', 'RURAL_RBD', 'COD_ENSE2', 'COD_GRADO', 'COD_JOR', 'GEN_ALU', 'EDAD_ALU', 'PROM_GRAL', 'ASISTENCIA']]
-data = data_all[columnas_seleccionadas]
-data.head()
+data_clear = data_all[columnas_seleccionadas]
+data = data_clear.drop('FEC_NAC_ALU', axis=1)
+data = data.drop('COD_GRADO', axis=1)
+
+print(data.columns)
 
 # División de datos
 X = data.drop(columns=['PROM_GRAL'])
@@ -71,9 +81,7 @@ X_test_scaled = scaler.transform(X_test)
 
 
 # GridSearch
-
-
-def build_regressor(optimizer, neurons1, neurons2, num_layers):
+def build_regressor(optimizer, neurons1, neurons2, num_layers, dropout_rate, learning_rate):
     regressor = Sequential()
     # Capa de entrada X_train_scaled.shape[1] = 9
     regressor.add(
@@ -82,33 +90,56 @@ def build_regressor(optimizer, neurons1, neurons2, num_layers):
     for i in range(num_layers):
         if i == 1:
             regressor.add(Dense(neurons1, activation='relu'))
-            regressor.add(Dropout(0.2))
+            regressor.add(Dropout(dropout_rate))
         elif i == 2:
             regressor.add(Dense(neurons2, activation='relu'))
-            regressor.add(Dropout(0.2))
+            regressor.add(Dropout(dropout_rate))
     regressor.add(Dense(1))
-    regressor.compile(optimizer=optimizer,
-                      loss='mae', metrics=['mse'])
+
+    if optimizer == 'adam':
+        opt = Adam(learning_rate=learning_rate)
+    elif optimizer == 'rmsprop':
+        opt = RMSprop(learning_rate=learning_rate)
+    elif optimizer == 'sgd':
+        opt = SGD(learning_rate=learning_rate)
+
+    # Compilación del modelo
+    regressor.compile(optimizer=opt, loss='mae', metrics=['mse'])
+
     return regressor
 
 
 param_grid = {
-    'neurons1': [64],  # [64, 128, 256, 512]
-    'neurons2': [32],  # [32, 64, 128, 256, 512]
-    'num_layers': [1],
-    'optimizer': ['adam'],
-    # [64, 128, 288, 424, 512, 1024] Cantidad de datos que se procesan antes de actualizar los pesos
-    'batch_size': [1024],
-    'validation_split': [0.3]
+    'neurons1': [32, 64, 128, 256],
+    'neurons2': [16, 32, 64],
+    'num_layers': [1, 2],
+    'optimizer': ['adam', "rmsprop", "sgd"],
+    # Cantidad de datos que se procesan antes de actualizar los pesos
+    'batch_size': [64, 128, 288, 424, 512, 1024],
+    'validation_split': [0.3],
+    'dropout_rate': [0.2, 0.3, 0.4],   # Tasas de dropout para probar
+    'learning_rate': [0.0001, 0.001, 0.01]  # Tasas de aprendizaje
 }
-# 4 * 3 * 2 * 1 * 6 = 144
+
+# 4 * 5 * 2 * 3 * 6 * 1 * 3 * 3 = 6480
+
+# param_grid = {
+#     'neurons1': [64],
+#     'neurons2': [32],
+#     'num_layers': [1],
+#     'optimizer': ['adam'],
+#     'batch_size': [1024], #Cantidad de datos que se procesan antes de actualizar los pesos
+#     'validation_split': [0.3]
+# }
+
 
 model = KerasRegressor(build_fn=build_regressor, verbose=1, epochs=15)
 
 grid_search = GridSearchCV(
-    estimator=model, param_grid=param_grid, cv=1, n_jobs=1)
+    estimator=model, param_grid=param_grid, cv=3, n_jobs=1)
 
-# 144 * 3 * 15 = 6480
+# 6480 * 3 * 15 = 291600
+
 
 # Entrenamiento
 grid_search = grid_search.fit(X_train_scaled, y_train)
@@ -139,3 +170,17 @@ with open('resultados/resultados_grid.txt', 'w') as file:
 model = grid_search.best_estimator_.model
 model.save('resultados/modelo.h5')
 model.save('resultados/modelo')  # Guardar modelo en formato SavedModel
+
+
+# Calcular el tiempo total de ejecución
+end_time = time.time()
+total_time = end_time - start_time
+
+# Imprimir el tiempo de ejecución
+print(f"Tiempo total de ejecución: {total_time:.2f} segundos")
+
+# imprimir tiempo en minutos
+print(f"Tiempo total de ejecución: {total_time/60:.2f} minutos")
+
+# imprimir tiempo en horas
+print(f"Tiempo total de ejecución: {total_time/3600:.2f} horas")
